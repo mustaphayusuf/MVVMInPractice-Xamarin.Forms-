08fe110a7f0286e2a3270b4eca31c9b11e89fcf4
Mustapha Yusuf <musty4000@gmail.com>
2020-05-06 02:51:29 -0700

    mvvm login

---
+++ XF_Login/XF_Login/View/XF_LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF_Login.ViewModel;
+
+namespace XF_Login.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XF_LoginPage : ContentPage
+    {
+        LoginViewModel loginViewModel;
+        public XF_LoginPage()
+        {
+            loginViewModel = new LoginViewModel();
+            InitializeComponent();
+            BindingContext = loginViewModel;
+        }
+
+    }
+}
---
+++ XF_Login/XF_Login/ViewModel/LoginViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XF_Login.View;
+
+namespace XF_Login.ViewModel
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginViewModel()
+        {
+            (NewCommand as Command).ChangeCanExecute();
+        }
+
+        public ICommand NewCommand { private set; get; }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get
+            {
+                return new Command(Login);
+            }
+        }
+
+
+        private void Login()
+        {
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+                App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
+            else
+            {
+                if (Email == "musty4000@gmail.com" && Password == "12345")
+                {
+                    App.Current.MainPage.DisplayAlert("Login Success", "", "OK");
+                    App.Current.MainPage.Navigation.PushAsync(new WelcomePage());
+                }
+                else
+                {
+                    App.Current.MainPage.DisplayAlert("Login Fail", "Please enter correct Email and Password", "Ok");
+                }
+            }
+        }
+    }
+}
